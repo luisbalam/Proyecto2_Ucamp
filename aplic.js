e9e148a7701c0024f03a4f1399a276d0e2cc9ad0
luisbalam <luisbalam@gmail.com>
2021-09-14 19:21:21 -0500

    hasta problema con localStorage

--- aplic.js
+++ aplic.js
@@ -1,1 +1,37 @@
-console.log("Hola funcionando bien!!!")
+//variables globales
+
+const formularioUI=document.querySelector('#formulario'); //la diferencia es que lleva el # en el id
+const listaActividadesUI=document.getElementById('listaActividades');
+let arrayActividades=[]
+
+
+
+//funciones
+const CrearItem= (actividad) => {
+    let item={
+        actividad: actividad,
+        estado: false
+    }
+    arrayActividades.push(item);
+    return item
+}
+
+const GuardarDB = () => {
+    localStorage.setItem('rutina', JSON.stringify(arrayActividades)); //?????
+}
+//CrearItem('correr');
+//CrearItem('nadar');
+//console.log(arrayActividades);
+
+//eventListener
+formularioUI.addEventListener('submit', (e) => {
+    e.preventDefault(); //para que no se refresque el sitio web
+    let actividadUI = document.querySelector('#actividad').value;
+
+    CrearItem(actividadUI);
+    GuardarDB();
+    formularioUI.reset();
+});
+
+
+
